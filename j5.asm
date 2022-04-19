846c8b2c7c915e1b60a699ef29c418ceb745fb62
Jaswanth <81845691+Jaswanth28@users.noreply.github.com>
2022-04-19 23:05:07 +0530

    Add files via upload

---
+++ j5.asm
@@ -0,0 +1,16 @@
+data segment
+data ends
+code segment
+assume cs:code,ds:data
+begin:,ov ax,data
+mov ds,ax
+mov cx,10
+mov dl,48
+l1:mov ah,2
+int 21h
+inc dl
+loop l1
+mov ah,4ch
+int 21h
+code ends
+end begin
